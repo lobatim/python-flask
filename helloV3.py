1126dbe768d3c870373722465c51972d2edc1448
lobatim <paulotrelo@gmail.com>
2022-04-15 11:33:04 -0300

    forms added

--- helloV3.py
+++ helloV3.py
@@ -1,9 +1,11 @@
 # exemplo de flask com bootstrap
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-Bootstrap(app)
+app.config['SECRET_KEY'] = 'amigos para sempre'
 
 @app.route('/')
 def index():
@@ -14,6 +16,17 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=''
+    return render_template('name.html',
+                           name=name,
+                           form=form)
+
 # retornara uma pagina de erro caso de erro 404
 @app.errorhandler(404)
 def error(e):
@@ -22,3 +35,8 @@ def error(e):
 @app.errorhandler(500)
 def error500(e):
     return render_template('500.html'), 500
+
+# criando uma classe form para receber os dados no site
+class NameForm(FlaskForm):
+    name = StringField("Qual e o seu nome?", validators=[DataRequired()])
+    submit = SubmitField("Entrar")
