b22f339e74afbfc52fe2499cb11ccd4ad334c177
lobatim <paulotrelo@gmail.com>
2022-04-15 15:16:04 -0300

    message added when form is submited

--- helloV3.py
+++ helloV3.py
@@ -1,5 +1,5 @@
 # exemplo de flask com bootstrap
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -9,6 +9,8 @@ app.config['SECRET_KEY'] = 'amigos para sempre'
 
 @app.route('/')
 def index():
+    # apresenta uma mensagem de boas vindas
+    flash("Seja bem vindo ao web site")
     return render_template('index.html')
 
 
@@ -16,13 +18,20 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+# criando uma rota com metodo get e post, esses metodos servem para obter dados de um site e passar dados para esse
+# site novamente.
 @app.route('/name', methods=['GET', 'POST'])
 def name():
     name = None
     form = NameForm()
+
+    # essa condiçao verifica se o botao submit foi pressionado
     if form.validate_on_submit():
+        # atribuindo os dados de form para name
         name = form.name.data
+        # definindo os dados do form para nenhum, para que esse seja capaz de receber novos dados.
         form.name.data=''
+        flash(" Seus dados foram carregados com sucesso !")
     return render_template('name.html',
                            name=name,
                            form=form)
@@ -32,11 +41,12 @@ def name():
 def error(e):
     return render_template('404.html'), 404
 
+# retornara uma pagina de erro caso de erro 500
 @app.errorhandler(500)
 def error500(e):
     return render_template('500.html'), 500
 
-# criando uma classe form para receber os dados no site
+# criando uma classe form, essa classe ira gerenciar a entrada de textos na form da pagina
 class NameForm(FlaskForm):
     name = StringField("Qual e o seu nome?", validators=[DataRequired()])
     submit = SubmitField("Entrar")
